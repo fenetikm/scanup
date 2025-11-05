7147706780033372ecafe1d133487c082d88a156
Michael Welford <mail@michaelwelford.com>
2025-11-05 20:19:42 +1030

    add: temp version test.

--- internal/commands/command.go
+++ internal/commands/command.go
@@ -6,6 +6,5 @@ import (
 
 type Command interface {
 	Run(state *internal.State) error
-	Name() string
 	Help() string
 }
---
+++ internal/commands/version/version_test.go
@@ -0,0 +1,13 @@
+package commands
+
+import (
+	"testing"
+	// "github.com/fenetikm/scanup/internal"
+	// "github.com/stretchr/testify/assert"
+	// "github.com/stretchr/testify/require"
+)
+
+func TestVersion(t *testing.T) {
+	// state := internal.State{Version: "1"}
+}
+
